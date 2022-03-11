0e537c69b524dc474e820f5f4d45fb824feed81f
ssccrypto <87320298+ssccrypto@users.noreply.github.com>
2022-03-11 08:49:21 -0500

    Delete BEP20.sol

--- BEP20.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: Unlicensed
-
-pragma solidity ^0.7.4;
-
-interface BEP20 {
-    function balanceOf(address) external returns (uint);
-    function transferFrom(address, address, uint) external returns (bool);
-    function transfer(address, uint) external returns (bool);
-}
