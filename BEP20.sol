4dbf228f09df842e12e993728b42ff649a460385
ssccrypto <87320298+ssccrypto@users.noreply.github.com>
2022-03-11 08:50:36 -0500

    Add files via upload

---
+++ BEP20.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.7.4;
+
+interface BEP20 {
+    function balanceOf(address) external returns (uint);
+    function transferFrom(address, address, uint) external returns (bool);
+    function transfer(address, uint) external returns (bool);
+}
---
+++ IDEXFactory.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.7.4;
+
+interface IDEXFactory {
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+}
