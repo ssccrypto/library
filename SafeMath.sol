e4fb316242e45aa416a1f4e55afe37b2cd149368
ssccrypto <87320298+ssccrypto@users.noreply.github.com>
2022-06-20 13:48:53 -0400

    Update SafeMath.sol

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,39 +1,23 @@
-pragma solidity ^0.7.4;
-
 library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-
-        return c;
-    }
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {return a + b;}
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {return a - b;}
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {return a * b;}
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {return a / b;}
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {return a % b;}
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {uint256 c = a + b; if(c < a) return(false, 0); return(true, c);}}
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {if(b > a) return(false, 0); return(true, a - b);}}
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {if (a == 0) return(true, 0); uint256 c = a * b;
+        if(c / a != b) return(false, 0); return(true, c);}}
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {if(b == 0) return(false, 0); return(true, a / b);}}
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {if(b == 0) return(false, 0); return(true, a % b);}}
     function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-
-        return c;
-    }
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-
-        return c;
-    }
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
-    }
+        unchecked{require(b <= a, errorMessage); return a - b;}}
     function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-
-        return c;
-    }
-}
+        unchecked{require(b > 0, errorMessage); return a / b;}}
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        unchecked{require(b > 0, errorMessage); return a % b;}}}
